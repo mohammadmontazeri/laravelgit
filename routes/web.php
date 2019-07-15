10381c19d7122094fb189dee4b92700c41300785
Your Actual Name <Your Actual Email>
2019-07-15 17:57:36 +0430

    Test

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/edituser', function () {
+    return view('user.edit');
+})->name('edit');
+
+Auth::routes();
+Route::post('/updateuser', 'UserController@update')->name('updateuser');
+
+Route::get('/home', 'HomeController@index')->name('home');
